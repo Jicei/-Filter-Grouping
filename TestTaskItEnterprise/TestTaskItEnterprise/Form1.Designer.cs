140c4534c7a3cd65fa6be19206bfe1293dfba771
Jicei <31828588+Jicei@users.noreply.github.com>
2019-11-13 21:14:33 +0200

    Add files via upload

---
+++ TestTaskItEnterprise/TestTaskItEnterprise/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TestTaskItEnterprise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chbDate = new System.Windows.Forms.CheckBox();
+            this.chbOrg = new System.Windows.Forms.CheckBox();
+            this.chbCountry = new System.Windows.Forms.CheckBox();
+            this.chbSity = new System.Windows.Forms.CheckBox();
+            this.chbMan = new System.Windows.Forms.CheckBox();
+            this.btnGroup = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(86, 85);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(755, 430);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // chbDate
+            // 
+            this.chbDate.AutoSize = true;
+            this.chbDate.Location = new System.Drawing.Point(906, 119);
+            this.chbDate.Margin = new System.Windows.Forms.Padding(6);
+            this.chbDate.Name = "chbDate";
+            this.chbDate.Size = new System.Drawing.Size(67, 28);
+            this.chbDate.TabIndex = 1;
+            this.chbDate.Text = "Date";
+            this.chbDate.UseVisualStyleBackColor = true;
+            // 
+            // chbOrg
+            // 
+            this.chbOrg.AutoSize = true;
+            this.chbOrg.Location = new System.Drawing.Point(906, 159);
+            this.chbOrg.Margin = new System.Windows.Forms.Padding(6);
+            this.chbOrg.Name = "chbOrg";
+            this.chbOrg.Size = new System.Drawing.Size(135, 28);
+            this.chbOrg.TabIndex = 2;
+            this.chbOrg.Text = "Organisation";
+            this.chbOrg.UseVisualStyleBackColor = true;
+            // 
+            // chbCountry
+            // 
+            this.chbCountry.AutoSize = true;
+            this.chbCountry.Location = new System.Drawing.Point(906, 199);
+            this.chbCountry.Margin = new System.Windows.Forms.Padding(6);
+            this.chbCountry.Name = "chbCountry";
+            this.chbCountry.Size = new System.Drawing.Size(94, 28);
+            this.chbCountry.TabIndex = 3;
+            this.chbCountry.Text = "Country";
+            this.chbCountry.UseVisualStyleBackColor = true;
+            // 
+            // chbSity
+            // 
+            this.chbSity.AutoSize = true;
+            this.chbSity.Location = new System.Drawing.Point(906, 239);
+            this.chbSity.Margin = new System.Windows.Forms.Padding(6);
+            this.chbSity.Name = "chbSity";
+            this.chbSity.Size = new System.Drawing.Size(58, 28);
+            this.chbSity.TabIndex = 4;
+            this.chbSity.Text = "Sity";
+            this.chbSity.UseVisualStyleBackColor = true;
+            // 
+            // chbMan
+            // 
+            this.chbMan.AutoSize = true;
+            this.chbMan.Location = new System.Drawing.Point(906, 279);
+            this.chbMan.Margin = new System.Windows.Forms.Padding(6);
+            this.chbMan.Name = "chbMan";
+            this.chbMan.Size = new System.Drawing.Size(104, 28);
+            this.chbMan.TabIndex = 5;
+            this.chbMan.Text = "Manager";
+            this.chbMan.UseVisualStyleBackColor = true;
+            // 
+            // btnGroup
+            // 
+            this.btnGroup.Location = new System.Drawing.Point(906, 500);
+            this.btnGroup.Margin = new System.Windows.Forms.Padding(6);
+            this.btnGroup.Name = "btnGroup";
+            this.btnGroup.Size = new System.Drawing.Size(238, 90);
+            this.btnGroup.TabIndex = 6;
+            this.btnGroup.Text = "Group";
+            this.btnGroup.UseVisualStyleBackColor = true;
+            this.btnGroup.Click += new System.EventHandler(this.BtnGroup_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(902, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Group By:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 750);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGroup);
+            this.Controls.Add(this.chbMan);
+            this.Controls.Add(this.chbSity);
+            this.Controls.Add(this.chbCountry);
+            this.Controls.Add(this.chbOrg);
+            this.Controls.Add(this.chbDate);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "TestTask";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.CheckBox chbDate;
+        private System.Windows.Forms.CheckBox chbOrg;
+        private System.Windows.Forms.CheckBox chbCountry;
+        private System.Windows.Forms.CheckBox chbSity;
+        private System.Windows.Forms.CheckBox chbMan;
+        private System.Windows.Forms.Button btnGroup;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TestTaskItEnterprise/TestTaskItEnterprise/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestTaskItEnterprise
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+
+        int countGroup;
+        int columnCount = 7;
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ItEnterpriseShipment.mdf;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            SqlDataReader sqlReader = null;
+
+            sqlConnection = new SqlConnection(connectionString);
+
+            await sqlConnection.OpenAsync();
+
+            SqlCommand command;
+
+            dataGridView1.ColumnCount = columnCount;
+            dataGridView1.RowCount = RowCount() + 1;
+
+            command = new SqlCommand("SELECT * FROM [Shipment]", sqlConnection);
+
+            sqlReader = await command.ExecuteReaderAsync();
+            
+            dataGridView1.Rows[0].Cells[0].Value = "Date";
+            dataGridView1.Rows[0].Cells[1].Value = "Organisation";
+            dataGridView1.Rows[0].Cells[2].Value = "Country";
+            dataGridView1.Rows[0].Cells[4].Value = "Sity";
+            dataGridView1.Rows[0].Cells[3].Value = "Manager";
+            dataGridView1.Rows[0].Cells[5].Value = "Quantity";
+            dataGridView1.Rows[0].Cells[6].Value = "Amount";
+
+            int counter = 0;
+
+            while (await sqlReader.ReadAsync())
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    dataGridView1.Rows[1 + counter].Cells[i].Value = Convert.ToString(sqlReader[i+1]);
+                }
+                counter++;
+            }
+
+            if (sqlReader != null)
+                sqlReader.Close();
+        }
+        private void BtnGroup_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.ColumnCount = columnCount;
+            dataGridView1.RowCount = RowCount() + 1;
+
+            string groupQuery = "";
+
+                if (chbDate.Checked == true)
+                {
+                    groupQuery = groupQuery + " Date";
+                }
+
+                if (chbOrg.Checked == true)
+                {
+                    groupQuery = groupQuery + " Organisation";
+                }
+
+                if (chbCountry.Checked == true)
+                {
+                    groupQuery = groupQuery + " Country";
+                }
+
+                if (chbSity.Checked == true)
+                {
+                    groupQuery = groupQuery + " Sity";
+                }
+
+                if (chbMan.Checked == true)
+                {
+                    groupQuery = groupQuery + " Manager";
+                }
+
+            if (groupQuery == "")
+            {
+                MessageBox.Show("Fill group by");
+                return;
+            }
+
+            string[] arrGroup = groupQuery.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < arrGroup.Length; i++)
+            {
+                dataGridView1.Rows[0].Cells[i].Value = arrGroup[i];
+            }
+            dataGridView1.Rows[0].Cells[arrGroup.Length].Value = "Quantity";
+            dataGridView1.Rows[0].Cells[arrGroup.Length  + 1].Value = "Amount";
+
+            string group = string.Join(",", arrGroup);
+
+            int counter = 0;
+
+            sqlConnection = new SqlConnection(connectionString);
+
+            sqlConnection.Open();
+
+            SqlDataReader sqlReader = null;
+
+            string query = "SELECT " + group + " ,sum(Quantity), sum(Amount) FROM [Shipment]" +
+                            " GROUP BY " + group;
+
+            SqlCommand command = new SqlCommand(query, sqlConnection);
+
+            sqlReader = command.ExecuteReader();
+
+            while (sqlReader.Read())
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    try
+                    {
+                        dataGridView1.Rows[1 + counter].Cells[i].Value = Convert.ToString(sqlReader[i]);
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+                counter++;
+            }
+
+            if (sqlReader != null)
+                sqlReader.Close();
+        }
+
+        int RowCount()
+        {
+
+            SqlCommand command = new SqlCommand("SELECT count(*) FROM [Shipment]", sqlConnection);
+
+            return Convert.ToInt32(command.ExecuteScalar());
+
+        }
+    }
+}
